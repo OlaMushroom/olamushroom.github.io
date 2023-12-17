3f34ebeb81eef246919267f76b969921bddf86ca
Nyanto <ola.mushroom@gmail.com>
2023-12-17 22:08:25 +0700

    fix

--- clickup/script.js
+++ clickup/script.js
@@ -1,5 +1,4 @@
-let list_id = ""
-
+const list_id = ""
 async function getResponse (name, desc = '',dt_start, dt_end) {
   const resp = await fetch(
     `https://api.clickup.com/api/v2/list/${list_id}/task`, {
@@ -8,7 +7,6 @@ async function getResponse (name, desc = '',dt_start, dt_end) {
         'Content-Type': 'application/json',
         Authorization: ''
       },
-
       body: JSON.stringify({
         name: name,
         description: desc,
@@ -23,22 +21,18 @@ async function getResponse (name, desc = '',dt_start, dt_end) {
   );
   return (await resp.json());
 }
-
 const form = document.getElementById("form");
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   document.getElementById("submit").innerHTML = `<span class="loading loading-infinity loading-lg"></span>`;
-
   const task = document.getElementById("task").value;
   const desc = document.getElementById("desc").value;
   const dt_start = document.getElementById("dt_start").value;
   const dt_end = document.getElementById("dt_end").value;
-
   try {
     const data = getResponse(task, desc, dt_start, dt_end);
     console.log(data);
   } catch (error) { console.error(error); }
-
   document.getElementById("output_task").innerHTML = `Task: ${task}`
   document.getElementById("output_desc").innerHTML = `Description: ${desc}`
   document.getElementById("output_dt_start").innerHTML = `Start date: ${dt_start}`
